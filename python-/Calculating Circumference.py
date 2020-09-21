f5ec9cf127317be4aa4d55f7f26ce342f33b86f1
VIK369 <67011579+VIK369@users.noreply.github.com>
2020-09-21 12:04:09 +0200

    Add files via upload

---
+++ python-/Calculating Circumference.py
@@ -0,0 +1,6 @@
+Radius=int(input("Whats The Radius of your Circle"))
+
+pi= 3.1415
+
+Circumference= pi*Radius*2
+print("The Circumference is \n",Circumference) 
---
+++ python-/Day 1 of Python.py
@@ -0,0 +1,18 @@
+print("Hello world")
+
+class_name ="class 2"
+
+print("I was coded in " + class_name)
+
+class_student = "19"
+
+print("There are "+ class_student+ " student in " + class_name)
+
+# Calculating the Cicumferance of the circle
+  
+Radius=int(input("Whats The Radius of your Circle"))
+
+pi= 3.1415
+
+Circumference= pi*Radius*2
+print("The Circumference is",Circumference)
